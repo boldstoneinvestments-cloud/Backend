11e3264af40af0c7fa15f829364bb45ad1c477da
Namatashabira <ssematasabira24@gmail.com>
2026-09-18 12:14:11 +0300

    Bootstrap admin account during migrations

---
+++ api/migrations/0002_bootstrap_admin.py
@@ -0,0 +1,46 @@
+import os
+
+from django.db import migrations
+
+
+def create_admin(apps, schema_editor):
+    username = os.getenv('ADMIN_USERNAME', '').strip()
+    email = os.getenv('ADMIN_EMAIL', '').strip()
+    password = os.getenv('ADMIN_PASSWORD', '')
+    if not username or not password:
+        return
+
+    User = apps.get_model('auth', 'User')
+    user, created = User.objects.get_or_create(
+        username=username,
+        defaults={
+            'email': email,
+            'is_staff': True,
+            'is_superuser': True,
+        },
+    )
+    if created:
+        user.set_password(password)
+        user.save(update_fields=['password'])
+    else:
+        fields = []
+        if email and user.email != email:
+            user.email = email
+            fields.append('email')
+        if not user.is_staff:
+            user.is_staff = True
+            fields.append('is_staff')
+        if not user.is_superuser:
+            user.is_superuser = True
+            fields.append('is_superuser')
+        if fields:
+            user.save(update_fields=fields)
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('api', '0001_initial'),
+        ('auth', '0012_update_proxy_permissions'),
+    ]
+
+    operations = [migrations.RunPython(create_admin, migrations.RunPython.noop)]
