4bcad87ad01b3fb2ce9684b646cd421f8d4196cb
Namatashabira <ssematasabira24@gmail.com>
2026-09-23 16:00:48 +0300

    Update

--- api/urls.py
+++ api/urls.py
@@ -3,10 +3,15 @@ from . import views
 
 urlpatterns = [
     path('health', views.health),
+    path('account/sign-up', views.account_signup),
+    path('account/sign-in', views.account_login),
+    path('account/me', views.account_me),
+    path('account/sign-out', views.account_logout),
     path('estate', views.estate),
     path('estate/invest', views.invest),
     path('orders', views.orders),
     path('contact', views.contact),
     path('chat', views.chat),
+    path('chat/stream', views.chat_stream),
     path('lease-applications', views.lease_applications),
 ]
